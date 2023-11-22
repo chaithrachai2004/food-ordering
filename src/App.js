c804b6d704bd6785e9a76859c2a2d0704932ca7b
chaithrachai2004 <chaithrachaip2004@gmail.com>
2023-11-22 21:00:25 +0530

    msg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+ 
+import React, { useState } from 'react';
+import FoodItem from './Components/FoodItem';
+ import Cart from './Components/Cart';
 
-function App() {
+ const foodMenu = [
+  { id: 1, name: 'Burger', price: 10.99 },
+  { id: 2, name: 'Pizza', price: 12.99 },
+// //   // Add more food items as needed
+ ];
+
+ const App = () => {
+  const [cartItems, setCartItems] = useState([]);
+
+ const addToCart = (item) => {
+   setCartItems([...cartItems, item]);
+ };
+
+  const removeFromCart = (index) => {
+   const newCartItems = [...cartItems];
+     newCartItems.splice(index, 1);
+   setCartItems(newCartItems);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+     <h1>Food Order Website</h1>
+    <div>
+       <h2>Menu</h2>
+      {foodMenu.map((item) => (
+          <FoodItem
+            key={item.id}
+           name={item.name}
+           price={item.price}
+          onAddToCart={() => addToCart(item)}
+        />
+        ))}
+     </div>
+    <Cart cartItems={cartItems} onRemoveFromCart={removeFromCart} />
+  </div>
   );
-}
+};
 
-export default App;
+ export default App;
+
+
+//   // src/App.js
+//   import React, { useState } from 'react';
+//   import './App.css';
+  
+//   const foodMenu = [
+//     { id: 1, name: 'Burger', price: 10.99 },
+//     { id: 2, name: 'Pizza', price: 12.99 },
+//     { id: 3, name: 'Salad', price: 8.99 },
+//     // Add more food items as needed
+//   ];
+  
+//   const App = () => {
+//     const [cartItems, setCartItems] = useState([]);
+  
+//     const addToCart = (item) => {
+//       setCartItems([...cartItems, item]);
+//     };
+  
+//     const removeFromCart = (index) => {
+//       const newCartItems = [...cartItems];
+//       newCartItems.splice(index, 1);
+//       setCartItems(newCartItems);
+//     };
+  
+//     const calculateTotal = () => {
+//       return cartItems.reduce((total, item) => total + item.price, 0).toFixed(2);
+//     };
+  
+//     return (
+//       <div className="app-container">
+//         <h1>Food Order Website</h1>
+//         <div className="menu-container">
+//           <h2>Menu</h2>
+//           {foodMenu.map((item) => (
+//             <div className="food-item" key={item.id}>
+//               <h3>{item.name}</h3>
+//               <p>${item.price.toFixed(2)}</p>
+//               <button onClick={() => addToCart(item)}>Add to Cart</button>
+//             </div>
+//           ))}
+//         </div>
+//         <div className="cart-container">
+//           <h2>Shopping Cart</h2>
+//           <ul>
+//             {cartItems.map((item, index) => (
+//               <li key={index}>
+//                 {item.name} - ${item.price.toFixed(2)}{' '}
+//                 <button onClick={() => removeFromCart(index)}>Remove</button>
+//               </li>
+//             ))}
+//           </ul>
+//           <div className="order-summary">
+//             <h3>Total: ${calculateTotal()}</h3>
+//             <button className="order-button">Place Order</button>
+//             </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
